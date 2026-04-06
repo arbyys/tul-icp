70a1cd6fb7665e59454c9c0962912ddcd1273ffc
Ondřej Šimůnek <ondrej.simunek1@tul.cz>
2026-04-06 15:40:29 +0200

    Fix texture parameters

--- src/shaders/Texture.cpp
+++ src/shaders/Texture.cpp
@@ -86,11 +86,11 @@ Texture::Texture(const std::vector<std::filesystem::path>& paths, Interpolation 
         }
     }
     glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    glTextureParameteri(name_, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTextureParameteri(name_, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTextureParameteri(name_, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTextureParameteri(name_, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTextureParameteri(name_, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 }
 
 Texture::Texture(const glm::vec3& vec) : Texture{ cv::Mat{1, 1, CV_8UC3, cv::Scalar{vec.b, vec.g, vec.r}}, Interpolation::nearest } {}
